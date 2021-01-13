6e04d7cc7f842fb782fd61f7415ef11f17cf0437
inps <fevany@163.com>
2021-01-13 09:29:59 +0800

    run

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ###  install
 1. python3 -m venv env
+   .\env\Scripts\activate
 2. ctrl shift  p : Python: Select Interpreter 
 3. pip3 install flask
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+@app.route('/')
+def index():
+    return 'Hello World'
+if __name__ == '__main__':
+    app.debug = True 
+    app.run()
